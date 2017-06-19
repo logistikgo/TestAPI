3791ebdcaf2fe44476446914a186428dd410caf1
imorales-logistikgo <imorales@logistikgo.com>
2017-06-19 15:01:25 -0500

    .

--- app/src/main/java/com/logistikgo/imorales/testapi/MainActivity.java
+++ app/src/main/java/com/logistikgo/imorales/testapi/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//hihi
+//hihi_hihi
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
