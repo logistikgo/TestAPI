5484e7bcb26edab67548fce795be7a72507fb58a
uriel gaytan <urielgaytanjimenez@gmail.com>
2017-06-19 14:56:39 -0500

    un punto

--- app/src/main/java/com/logistikgo/imorales/testapi/MainActivity.java
+++ app/src/main/java/com/logistikgo/imorales/testapi/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+//hi
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
